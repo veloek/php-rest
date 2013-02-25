0a902d33cef9ccedb92e3621086d6246cb0b4ff8
Vegard Løkken <vegard@loekken.org>
2013-02-25 15:16:56 +0100

    Added a html form for file upload example testing

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ require_once('rest' . DIRECTORY_SEPARATOR . 'Server.php');
 /**
  * Main file for examples. Instantiates the REST server implementation
  * and registeres the services in the examples folder.
+ *
+ * To start creating you own web services, replace example services
+ * with you own services and follow the given structure.
+ *
+ * See more at https://github.com/veloek/php-rest
  */
 
 $server = new Server("phpREST Example Web Services");
