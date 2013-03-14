ac30d6ecfac4e2992a3c2ffaf6f5d5888e2f0577
Vegard Løkken <vegard@loekken.org>
2013-03-14 12:16:44 +0100

    Add ob_start() and ob_flush() to solve header warnings

--- rest/Server.php
+++ rest/Server.php
@@ -19,6 +19,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+ob_start();
 
 require_once('Response.php');
 require_once('Request.php');
@@ -420,6 +421,8 @@ class Server {
     }
     
     echo $this->response->getContent();
+    
+    ob_flush();
   }
 }
 
